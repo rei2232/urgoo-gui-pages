7bac75546227ad28414715c1abf4bb9fb09dcc22
Dashjamts <hustle2232@gmail.com>
2022-05-26 22:32:14 +0800

    .

--- server.js
+++ server.js
@@ -2,13 +2,19 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const router = express.Router()
-
 app.use(express.static('public'))
 app.get('/movie/:id',function(req,res){
-    console.log(req.params.id)
     res.sendFile(path.join(__dirname+'/public/movie.html'))
 });
 router.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/index.html'))
 });
+app.get('/api/movie', (req, res) => {
+    res.header("Content-Type",'application/json')
+    res.sendFile(path.join(__dirname + '/public/data/movie.json'))
+})
+app.get('/api/movie/:id', (req, res) => {
+    // res.header("Content-Type",'application/json')
+    // res.sendFile(path.join(__dirname + '/public/data/movie.json')
+})
 app.listen(process.env.PORT || 3000, () => console.log("server is now running."))
